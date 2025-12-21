d1b0f4528692c52330bc5b71ae46d2076e65101d
Alessandro Rizzo <alessandro.rizzo@ki-insurance.com>
2025-12-21 18:18:00 +0000

    feat: add dynamic rendering for VideosPage to handle large video data

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -2,6 +2,9 @@ import { prisma } from "@/lib/db";
 import { VideoList } from "@/components/video-list";
 import { listRemoteInteractions, checkDownloadedInteractions } from "@/lib/dataset-remote";
 
+// Force dynamic rendering - page is too large to pre-render (64,000+ videos)
+export const dynamic = 'force-dynamic';
+
 export default async function VideosPage() {
   const [interactions, annotations] = await Promise.all([
     listRemoteInteractions().then(checkDownloadedInteractions),
